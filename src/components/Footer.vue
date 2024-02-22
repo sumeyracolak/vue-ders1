0192b5487670bd4c0fe6d5deef39cac3e2a94028
sumeyra <sumeyracolak123@gmail.com>
2024-02-22 21:27:00 +0300

    vue desr1

---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<script setup>
+    import { RouterLink, RouterView } from 'vue-router'
+</script>
+<template>
+    <footer>
+        <ul>
+            <li><a href="#">Hakkımızda</a></li>
+            <li><a href="#">İletişim</a></li>
+            <li><a href="#">Gizlilik Politikası</a></li>
+            <li><a href="#">Kullanım Şartları</a></li>
+        </ul>
+    </footer>
+</template>
+<style scoped lang="scss">
+    footer{
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: #333;
+        ul{
+            margin: 0;
+            padding: 0;
+            list-style: none;
+            display: flex;
+            justify-content: center;
+            gap: 20px;
+            
+            a{
+                color: #fff;
+                padding: 5px;
+                display: block;
+            }
+        }
+    }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<script setup>
+    import { RouterLink, RouterView } from 'vue-router'
+</script>
+<template>
+    <header>
+        <RouterLink to="/">Anasayfa</RouterLink>
+        <RouterLink to="/about">Hakkımızda</RouterLink>
+      </header>
+</template>
+<style scoped lang="scss">
+    header{
+        position: absolute;
+        left: 0;
+        top: 0;
+        right: 0;
+        display: flex;
+        padding: 20px;
+        gap: 20px;
+        a{
+            display: block;
+            color: #000;
+        }
+    }
+</style>
